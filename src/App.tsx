67bb3998ba63e8d284c879e003c2ac6ba82e9043
Igor <evildadaya@gmail.com>
2025-07-08 00:08:31 +0300

    v 0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './img/logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content-box">
+        <header><p>kxtfn</p></header>
+    
+        <main>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Welcome!</h1>
+          <p>You've landed on my personal page — a place where I showcase side projects, 
+          fun features, and random things I'm working on.</p>
+          <h1>About me</h1>
+          <p>This is a simple React application.</p>
+        </main>
+    
+        <footer>
+          <p>© 2025 Igor</p>
+        </footer>
+      </div>
     </div>
   );
+
 }
 
 export default App;
